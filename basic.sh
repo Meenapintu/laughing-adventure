4f8d4ee8ec6d1bf48b67936f4e38dcf92270091b
Pintu Lal M <pintulalmeena@hotmail.com>
2016-05-03 07:02:11 +0530

    ubuntu 16.04 LTS

--- basic.sh
+++ basic.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get dist-upgrade
 sudo apt-get install build-essential geany nginx php mysql-server php-mysql  vim dconf-editor git cmake
